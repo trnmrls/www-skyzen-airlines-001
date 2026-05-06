2e5e24b424304cf9b3cc2bcabf5f82f97c4fbeee
trnmrls <trine.mrls@gmail.com>
2026-05-06 21:48:27 +0800

    Sixth Commit: Updated Password validation as well as inserted a confirmation upon registration

--- scripts/service.js
+++ scripts/service.js
@@ -117,9 +117,23 @@ document.addEventListener('DOMContentLoaded', function() {
         return;
     }
 
-    if (payload.users_password.length < 8) {
-        Swal.fire({ icon: 'warning', title: 'Weak Password', text: 'Password must be at least 8 characters long.' });
+    var confirmPassword = $('#regConfirmPassword').val();
+    var passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*_)[A-Za-z\d_]{8,}$/;
+    if (!passwordPattern.test(payload.users_password)) {
+        Swal.fire({ 
+            icon: 'warning', 
+            title: 'Weak Password', 
+            text: 'Password must be at least 8 characters long and MUST contain a mix of letters, numbers, and an underscore (_).' 
+        });
         return;
+    }
+    if (payload.users_password !== confirmPassword) {
+        Swal.fire({ 
+            icon: 'warning', 
+            title: 'Passwords Mismatch', 
+            text: 'Your passwords do not match. Please re-type them carefully!' 
+        });
+        return; 
     }
 
     $.ajax({
